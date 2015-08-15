f8b426f3948063b693cc37e9ce1984a9c6d2996c
sarah-maris <sarah.87.maris@gmail.com>
2015-08-15 02:35:00 -0400

    Add concat, sourcemap and uncss to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,52 @@ var jshint = require('gulp-jshint');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var imageminJpegRecompress = require('imagemin-jpeg-recompress');
+var uncss = require('gulp-uncss');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
 var ghPages = require('gulp-gh-pages');
+
+// Paths to files
+var paths = {
+    pizzascripts: ['views/js/*.js'],
+    pizzastyles: ['views/css/**/*.css'],
+    pizzaimages: ['views/images/*'],
+    pizzajs: ['views/js/*.js']
+}
 
 //Minify js
 gulp.task('scripts', function(){
-    gulp.src('views/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('views/js/app.min.js'));
+    return gulp.src(paths.pizzascripts)
+        .pipe(sourcemaps.init())
+            .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('views/js/app.min.js/'));
 });
 
-//Minify css
+//Concatenate, remove unused styles and minify css
 gulp.task('styles', function(){
-    gulp.src('views/css/**/*.css')
-        .pipe(minifyCSS())
+    return gulp.src(paths.pizzastyles)
+        .pipe(sourcemaps.init())
+            .pipe(concat('main.css'))        
+            .pipe(uncss({
+                html: ['views/pizza.html'],
+                ignore: ['.mover'] //uncss doesn't see this in pizza.html because it is generated in main.js
+             }))
+            .pipe(minifyCSS())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('views/minCSS'));
 });
 
 //Lint js
 gulp.task('lint', function() {
-  return gulp.src('views/js/*.js')
+  return gulp.src(paths.pizzajs)
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
 });
 
 //Compress png images
 gulp.task('png-images', function() {
-  return gulp.src('views/images/*.png')
+  return gulp.src(paths.pizzaimages + '.png')
     .pipe(imagemin({
     progressive: true,
     svgoPlugins: [{removeViewBox: false}],
@@ -42,7 +62,7 @@ gulp.task('png-images', function() {
 
 //Compress jpg images
 gulp.task('jpg-images', function () {
-    return gulp.src('views/images/*.jpg')
+    return gulp.src(paths.pizzaimages + '.jpg')
         .pipe(imageminJpegRecompress({loops: 3})())
         .pipe(gulp.dest('views/build/images'));
 });
@@ -53,8 +73,9 @@ gulp.task('watch', function(){
     gulp.watch('js/*.js', function(event) {
        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
-    gulp.watch('views/css/**/*.css', ['styles']);
-    gulp.watch('views/js/*.js', ['lint']);
+    gulp.watch( paths.pizzastyles, ['styles']);
+    gulp.watch( paths.pizzajs, ['lint']);
+    gulp.watch( paths.pizzaimages, ['png-images','jpg-images']);
 });
 
 //Push changes to github pages
