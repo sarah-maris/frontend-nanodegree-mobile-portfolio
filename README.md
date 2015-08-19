78a65ea8e7003744784149f07c0388745b4612a4
sarah_maris <sarah.87.maris@gmail.com>
2015-08-19 18:24:37 -0400

    Modify gulpfile to minify CSS & JS and compress images (PSI M56/D61)

--- README.md
+++ README.md
@@ -4,31 +4,38 @@
 ####Part 1: Optimized PageSpeed Insights score for index.html
 1) Added media query (media="print") to print stylesheet link
 
+2) Minified CSS and JS
+   Used gulp-uncss to remove unneeded CSS and minified it
+   Minified js
+
+3) Compressed images
+   Used gulp imagemin pngs and imagemin-jpeg-recompress for jpgs
+
+
 ####Part 2: Optimized Frames per Second in pizza.html
-1) Added "will-change: tranform" to .movers to makes the pizzas separate layers to reduce 
+1) Added "will-change: tranform" to .movers to makes the pizzas separate layers to reduce
 
 2) Reduced number of moving pizzas to minimum needed to fill the frame
    Use window height and width to calculate the number of rows and columns needed
-   
+
 3) Fixed function to move sliding background pizzas more efficiently
    Moved scrollTop calculation out of loop to avoid forced synchronous layout
    Moved the "phase" calculation outside of the loop and store the five possible phases in an array
    Used "transform: translateX()" instead of "left" to avoid triggering layout and paint
    Replaced document.querySelectorAll() with more efficient document.getElementsByClassName()
    Moved document.getElementsByClassName() out of function so it is not calculated with each scroll
-   
+
 4) Adjusted "reSizePizzas" function to avoid forced synchronous layout
-   Simplified calculation to sets new pizza width as % of "randomPizzas" div 
+   Simplified calculation to sets new pizza width as % of "randomPizzas" div
    Replaced document.querySelectorAll() with more efficient document.getElementsByClassName()
    Moved document.getElementsByClassName() out of function so it is not calculated with each move
-   
+
 5) Compressed and reduced size of images to reduce load time.
-   Reduced width of pizzeria.jpg to 1440px 
+   Reduced width of pizzeria.jpg to 1440px
    Created mover-pizza.png to make smaller image for moving pizza and changed mover pizza with in main.js to match
-   Used gulp imagemin for pizza.png and mover-pizza.png and and imagemin-jpeg-recompress for pizzeria.jpg 
-   
+   Used gulp imagemin for pizza.png and mover-pizza.png and imagemin-jpeg-recompress for pizzeria.jpg
+
 6) Adjusted CSS
    Removed size from .mover -- set in main.js
    Concatenated style.css and bootstrap and then removed unneeded css with gulp-uncss
-   
-   
+
--- gulpfile.js
+++ gulpfile.js
@@ -13,26 +13,36 @@ var ghPages = require('gulp-gh-pages');
 
 // Paths to files
 var paths = {
-    pizzascripts: ['views/js/*.js'],
     pizzastyles: ['views/css/**/*.css'],
     pizzaimages: ['views/images/*'],
-    pizzajs: ['views/js/*.js']
+    pizzajs: ['views/js/*.js'],
+    portfoliostyles: ['css/**/*.css'],
+    portfolioimages: ['img/*'],
+    portfoliojs: ['js/*.js']
 }
 
 //Minify js
-gulp.task('scripts', function(){
-    return gulp.src(paths.pizzascripts)
+gulp.task('pizza-scripts', function(){
+    return gulp.src(paths.pizzajs)
         .pipe(sourcemaps.init())
             .pipe(uglify())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('views/js/app.min.js/'));
 });
 
+gulp.task('portfolio-scripts', function(){
+    return gulp.src(paths.portfoliojs)
+        .pipe(sourcemaps.init())
+            .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/minjs/'));
+});
+
 //Concatenate, remove unused styles and minify css
-gulp.task('styles', function(){
+gulp.task('pizza-styles', function(){
     return gulp.src(paths.pizzastyles)
         .pipe(sourcemaps.init())
-            .pipe(concat('main.css'))        
+            .pipe(concat('main.css'))
             .pipe(uncss({
                 html: ['views/pizza.html'],
                 ignore: ['.mover'] //uncss doesn't see this in pizza.html because it is generated in main.js
@@ -42,15 +52,32 @@ gulp.task('styles', function(){
         .pipe(gulp.dest('views/minCSS'));
 });
 
+gulp.task('portfolio-styles', function(){
+    return gulp.src(paths.portfoliostyles)
+        .pipe(sourcemaps.init())
+            .pipe(uncss({
+                html: ['*.html']
+             }))
+            .pipe(minifyCSS())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/mincss'));
+});
+
 //Lint js
-gulp.task('lint', function() {
+gulp.task('pizza-lint', function() {
   return gulp.src(paths.pizzajs)
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
 });
 
+gulp.task('portfolio-lint', function() {
+  return gulp.src(paths.portfoliojs)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+});
+
 //Compress png images
-gulp.task('png-images', function() {
+gulp.task('pizza-png-images', function() {
   return gulp.src(paths.pizzaimages + '.png')
     .pipe(imagemin({
     progressive: true,
@@ -60,22 +87,46 @@ gulp.task('png-images', function() {
   .pipe(gulp.dest('views/build/images'))
 })
 
+gulp.task('portfolio-png-images', function() {
+  return gulp.src(paths.portfolioimages + '.png')
+    .pipe(imagemin({
+    progressive: true,
+    svgoPlugins: [{removeViewBox: false}],
+    use: [pngquant()]
+    }))
+  .pipe(gulp.dest('build/optimg'))
+})
+
 //Compress jpg images
-gulp.task('jpg-images', function () {
+gulp.task('pizza-jpg-images', function () {
     return gulp.src(paths.pizzaimages + '.jpg')
         .pipe(imageminJpegRecompress({loops: 3})())
         .pipe(gulp.dest('views/build/images'));
 });
 
+gulp.task('portfolio-jpg-images', function () {
+    return gulp.src(paths.portfolioimages + '.jpg')
+        .pipe(imageminJpegRecompress({loops: 3})())
+        .pipe(gulp.dest('build/optimg'));
+});
+
+
 //Watch for changes
 gulp.task('watch', function(){
-    gulp.watch('js/*.js', ['scripts']);
-    gulp.watch('js/*.js', function(event) {
+    gulp.watch( paths.pizzajs, ['pizza-scripts']);
+    gulp.watch( paths.pizzajs, function(event) {
        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
-    gulp.watch( paths.pizzastyles, ['styles']);
-    gulp.watch( paths.pizzajs, ['lint']);
-    gulp.watch( paths.pizzaimages, ['png-images','jpg-images']);
+    gulp.watch( paths.pizzastyles, ['pizza-styles']);
+    gulp.watch( paths.pizzajs, ['pizza-lint']);
+    gulp.watch( paths.pizzaimages, ['pizza-png-images','pizza-jpg-images']);
+    gulp.watch( paths.portfoliojs, ['portfolio-scripts']);
+    gulp.watch( paths.portfoliojs, function(event) {
+       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+    gulp.watch( paths.portfoliostyles, ['portfolio-styles']);
+    gulp.watch( paths.portfoliojs, ['portfolio-lint']);
+    gulp.watch( paths.portfolioimages, ['portfolio-png-images','portfolio-jpg-images']);
 });
 
 //Push changes to github pages
@@ -84,4 +135,6 @@ gulp.task('deploy', function() {
     .pipe(ghPages());
 });
 
-gulp.task('default', ['scripts', 'styles', 'lint', 'png-images', 'jpg-images','watch']);
+gulp.task('pizza', ['pizza-scripts', 'pizza-styles', 'pizza-lint', 'pizza-png-images', 'pizza-jpg-images','watch']);
+
+gulp.task('portfolio', ['portfolio-scripts', 'portfolio-styles', 'portfolio-lint', 'portfolio-png-images', 'portfolio-jpg-images','watch']);
